3468bd5fe17d3c84375fd0fd0ec9ccb74f78e2a8
cindyweng672 <w1256cw@gmail.com>
2023-06-29 18:38:10 -0800

    Horizontal/Vertical/Diagonal check
    done
    6/15

--- 2023-J5.java/src/App.java
+++ 2023-J5.java/src/App.java
@@ -24,84 +24,26 @@ public class App {
             }
             System.out.println();
         }
-        System.out.println(horizontalCheck(target, grid));
+        System.out.println(wordHunt(target, grid));
     }
 
-    public static int horizontalCheck(String target, String[][] g) {
+    public static int wordHunt(String target, String[][] g) {
         int num = 0;
         String s = target.substring(0, 1);
         for (int i = 0; i < g.length; i++) {
             for (int j = 0; j < g[0].length; j++) {
                 if (g[i][j].equals(s)) {
-                    if (check(i, j, target, g, "hr")) {
-                        num++;
+                    int[] steps = { 0, -1, 1 };
+                    for (int k = 0; k < steps.length; k++) {
+                        for (int l = 0; l < steps.length; l++) {
+                            if (steps[k] == 0 && steps[l] == 0) {
+                                continue;
+                            }
+                            if(check(i, j, target, g, steps[k], steps[l])){
+                                num++;
+                            }
+                        }
                     }
-                    if (check(i, j, target, g, "hl")) {
-                        num++;
-                    }
-                    if (check(i, j, target, g, "vd")) {
-                        num++;
-                    }
-                    if (check(i, j, target, g, "vu")) {
-                        num++;
-                    }
-                    /*
-                     * int index = 1;
-                     * if ((j + target.length()) <= g[0].length + 1) {
-                     * for (int k = j + 1; k < j + target.length(); k++) {
-                     * if (!g[i][k].equals(target.substring(index, index + 1))) {
-                     * break;
-                     * } else {
-                     * index++;
-                     * }
-                     * }
-                     * if (index == target.length()) {
-                     * num++;
-                     * }
-                     * }
-                     * 
-                     * index = 1;
-                     * if ((j - target.length()) >= -1) {
-                     * for (int k = j - 1; k > j - target.length(); k--) {
-                     * if (!g[i][k].equals(target.substring(index, index + 1))) {
-                     * break;
-                     * } else {
-                     * index++;
-                     * }
-                     * }
-                     * if (index == target.length()) {
-                     * num++;
-                     * }
-                     * }
-                     * 
-                     * index = 1;
-                     * if ((i + target.length()) <= g.length + 1) {
-                     * for (int k = i + 1; k < i + target.length(); k++) {
-                     * if (!g[k][j].equals(target.substring(index, index + 1))) {
-                     * break;
-                     * } else {
-                     * index++;
-                     * }
-                     * }
-                     * if (index == target.length()) {
-                     * num++;
-                     * }
-                     * }
-                     * 
-                     * index = 1;
-                     * if ((i - target.length()) >= -1) {
-                     * for (int k = i - 1; k > i - target.length(); k--) {
-                     * if (!g[k][j].equals(target.substring(index, index + 1))) {
-                     * break;
-                     * } else {
-                     * index++;
-                     * }
-                     * }
-                     * if (index == target.length()) {
-                     * num++;
-                     * }
-                     * }
-                     */
 
                 }
             }
@@ -109,91 +51,38 @@ public class App {
         return num;
     }
 
-    public static boolean check(int i, int j, String target, String[][] g, String direction) {
+    public static boolean check(int i, int j, String target, String[][] g, int stepX, int stepY) {
         int index = 1;
         int roomRequiredX = 0;
         int roomRequiredY = 0;
 
-        int stepx = 0;
-        int stepy = 0;
+        roomRequiredX = j + stepX * target.length() - stepX;
+        roomRequiredY = i + stepY * target.length() - stepY;
 
-        if (direction.equals("hr")) {
-            stepx = 1;
-        } else if (direction.equals("hl")) {
-            stepx = -1;
-        } else if (direction.equals("vd")) {
-            stepy = 1;
-        } else if (direction.equals("vu")) {
-            stepy = -1;
-        }
+        int currentR = i;
+        int currentC = j;
 
-        roomRequiredX = j + stepx * target.length() - stepx;
-        roomRequiredY = i + stepy * target.length() - stepy;
-
-        if (roomRequiredX < g.length && roomRequiredX >= 0 && roomRequiredY < g[0].length && roomRequiredY >= 0) {
-            for (int count = 0; count < target.length()-1; count++) {
-                String checkingCell = g[i + stepy*(count+1)][j + stepx*(count+1)];
-                boolean matched = false;
+        if (roomRequiredX < g[0].length && roomRequiredX >= 0 && roomRequiredY < g.length && roomRequiredY >= 0) {
+            for (int count = 0; count < target.length() - 1; count++) {
+                currentR += stepY;
+                currentC += stepX;
+                String checkingCell = g[currentR][currentC];
+                String targetLetter = target.substring(index);
                 if (index < target.length()) {
-                    matched = checkingCell.equals(target.substring(index, index + 1));
-                } else {
-                    matched = checkingCell.equals(target.substring(index));
+                    targetLetter = target.substring(index, index + 1);
                 }
-                if(matched){
+                boolean matched = targetLetter.equals(checkingCell);
+                if (matched) {
                     index++;
-                }else{
-                    break;
+                } else {
+                    return false;
                 }
             }
 
-            if(index==target.length()){
+            if (index == target.length()) {
                 return true;
             }
-            /*
-             * for (int r = i + stepy; !(r > roomRequiredY + stepy || r < 0); r += stepy) {
-             * for (int c = j + stepx; !(c > j + roomRequiredX + stepx || c < 0); c +=
-             * stepx) {
-             * String checkingCell = g[r][c];
-             * boolean matched=false;
-             * if(index<4){
-             * matched=checkingCell.equals(target.substring(index, index + 1));
-             * }else{
-             * matched=checkingCell.equals(target.substring(index));
-             * }
-             * if (!matched) {
-             * break;
-             * } else {
-             * index++;
-             * }
-             * }
-             * if (stepy == 0) {
-             * break;
-             * }
-             * }
-             */
         }
-
-        /*
-         * if ((vh + roomRequired) <= roomHad) {
-         * for (int k = vh + indexDirection; k < vh + roomRequired; k += indexDirection)
-         * {
-         * String checkingCell = "";
-         * if (direction.equals("hl") || direction.equals("hr")) {
-         * checkingCell = g[i][k];
-         * } else {
-         * checkingCell = g[k][j];
-         * }
-         * if (!checkingCell.equals(target.substring(index, index + 1))) {
-         * break;
-         * } else {
-         * index++;
-         * }
-         * }
-         * if (index == target.length()) {
-         * return true;
-         * }
-         * }
-         */
 
         return false;
     }
